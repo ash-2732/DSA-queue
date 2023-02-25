3eb2503b953b6219fded85598379db4c8cca5e68
Abdullah Ashik <121299454+ash-2732@users.noreply.github.com>
2023-02-25 21:40:57 +0600

    Add files via upload

---
+++ queue.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+int const n = 100;
+using namespace std;
+
+
+class Queue{
+    int *arr;
+    int front;
+    int back;
+    public:
+    Queue(){
+        arr = new int[n];
+        front = -1;
+        back = -1;
+    }
+    void push( int x ){
+        if( back == n-1 ){
+            cout << "Queue overflow" << endl;
+            return;
+        }
+        back++;
+        arr[back] = x;
+        if( front == -1 ){
+            front++;
+        }
+    }
+    void pop(){
+        if( front == -1 || front > back ){
+            cout << "No element in Queue" << endl;
+            return;
+        }
+        front++;
+    }
+    int peek(){
+        if( front == -1 || front > back ){
+            cout << "No element in Queue" << endl;
+            return -1;
+        }
+        return arr[front];
+    }
+    bool empty(){
+        if( front == -1 || front > back ){
+            return true;
+        }
+        return false;
+    }
+};
+
+int main(){
+
+    Queue q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    cout << q.peek() << endl;
+    q.pop();
+    cout << q.peek() << endl;
+    q.pop();
+    cout << q.peek() << endl;
+    q.pop();
+    cout << q.peek() << endl;
+    q.pop();
+    cout << q.empty() << endl;
+
+    // output
+    //    1
+    //    2
+    //    3
+    //    4
+    //    1
+
+    return 0;
+}
